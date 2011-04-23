965eeb45cb0b8cc7dd5c9caa29aa6123423e0ba2
Jean-Philippe Daigle <jpdaigle@gmail.com>
2011-04-22 23:04:49 -0400

    initializing the eclipse project

---
+++ src/ca/softwareengineering/skilltimeline/Main.java
@@ -0,0 +1,12 @@
+package ca.softwareengineering.skilltimeline;
+
+/**
+ * Main entry point to the application: parses command line and provides app
+ * runtime.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Unimplemented. Exiting.");
+	}
+}
